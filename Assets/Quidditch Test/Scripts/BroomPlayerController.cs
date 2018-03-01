1f48bef2952d9e0de3367fa82fa90d3d5524f7aa
Shailesh Iyer <swethashailu9696@gmail.com>
2018-03-02 00:25:59 +0530

    Another commit for adding things the last one didn't
    In this commit the scene which i ran on my phone has
    been saved and is being pushed. Removed the rigid
    body from the player character which also removed
    the problem of incorrect collision due to physics
    (as in incorrect for us ) due to the physics
    interfering with the head tracking and breaking it
    due to incorrect rotation of the camera and it
    could potentially disorient users.
    
    The problem with removing the rigid body is movement
    now it does not trail off it just stops whenever
    we stop using the joystick which leaves an abrupt
    feel to it which is not suitable for a character in
    flight. Need to find the solution for this.
    Also added in this commit are the tree models created
    by sanoop and a prefab for the player object.

---
+++ Assets/Quidditch Test/Scripts/BroomPlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BroomPlayerController : MonoBehaviour {
+
+	public float speed= 40;
+	Vector2 TouchCoords;
+	CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> (); 		
+	
+	}
+
+	void GetInput(){
+
+		//heightInput = Input.GetAxis ("Vertical");
+		//turnInput = Input.GetAxis ("Horizontal");
+		//forwardInput = Input.GetKey (KeyCode.LeftShift);
+		if (GvrControllerInput.IsTouching) {
+			TouchCoords = GvrControllerInput.TouchPosCentered;
+
+			//transform.forward = Camera.main.transform.forward;
+			//transform.right = Camera.main.transform.right;
+			Vector3 Player_forward = Camera.main.transform.forward;
+			Vector3 Player_right = Camera.main.transform.right;
+
+			controller.Move (TouchCoords.y * Player_forward * speed*Time.deltaTime);
+			controller.Move (TouchCoords.x * Player_right * speed*Time.deltaTime);
+
+	//		rBody.velocity = TouchCoords.y * Player_forward * speed;
+	//		rBody.velocity += TouchCoords.x * Player_right * speed; 
+
+		} else {
+			//TouchCoords = new Vector2();
+		}
+
+		Debug.Log ("Tranform.forward" + transform.forward+"Transform.right"+transform.right);
+
+		//Debug.Log(TouchCoords+" velocity = "+rBody.velocity);
+	}
+	// Update is called once per frame
+	void Update () {
+		GetInput ();
+		//Turn();
+	}
+
+	void FixedUpdate(){
+		//Moveforward ();
+	}
+
+}
